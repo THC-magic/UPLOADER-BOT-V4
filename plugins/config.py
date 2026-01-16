521593ea354f5b90d7c3b54b8db90c2a71abfda4
THC-magic <vish@mailchat.me>
2026-01-16 23:05:22 +0530

    Update config.py

--- plugins/config.py
+++ plugins/config.py
@@ -26,7 +26,7 @@ class Config(object):
     OUO_IO_API_KEY = ""
     MAX_MESSAGE_LENGTH = 4096
     PROCESS_MAX_TIMEOUT = 3600
-    DEF_WATER_MARK_FILE = "@UploaderXNTBot"
+    DEF_WATER_MARK_FILE = "@ig_magic"
 
     ADMIN = set(
         int(x) for x in environ.get("ADMIN", "").split()
